1327dc1b56bd3844e0676940d69a1b3ce347fec3
leoarch <43377227+leoarch@users.noreply.github.com>
2019-02-10 20:55:37 -0200

    Add files via upload

--- xfce/custom.sh
+++ xfce/custom.sh
@@ -4,42 +4,28 @@ _g="\e[32;1m";_e="\e[m";_w="\e[37;1m";_y="\e[33;1m";
 
 echo -e "${_y}Instalando programas pacman\n${_e}"
 
-# programas normais
-program=(
-    'galculator'
-    #'mysql-workbench'
-    'gnome-keyring'
-    'git'
-    'flatpak'
-    'engrampa'
-)
-
-for list in "${program[@]}"; do
-    echo "==> Instalando programa: ${list}"; sleep 0.5
-    #pacman -S ${list} --noconfirm
-done
+sudo pacman -S galculator flatpak engrampa go conky --noconfirm
 
 echo -e "\n${_y}Instalando programas flatpak\n${_e}"
 
-# flatpaks
-flatpak=(
-    'com.axosoft.GitKraken'
-    'com.discordapp.Discord'
-    'org.qbittorrent.qBittorrent'
-    'org.telegram.desktop'
-    'org.videolan.VLC'
-    'org.remmina.Remmina'
-    'org.filezillaproject.Filezilla'
-)
+flatpak install com.discordapp.Discord org.telegram.desktop org.remmina.Remmina org.videolan.VLC org.filezillaproject.Filezilla com.anydesk.Anydesk com.dropbox.Client com.valvesoftware.Steam
 
-for list_flatpak in "${flatpak[@]}"; do
-    echo "==> Instalando programa: ${list_flatpak}"; sleep 0.5
-    #flatkpak install ${list_flatpak}
-done
+# install sublime-text
+echo -e "\n${_y}Instalando sublime-text\n${_e}"
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg; sleep 1
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf; sleep 1
+sudo pacman -Syu sublime-text
 
-# AUR
+# instalando programas AUR
+echo -e "\n${_y}Instalando yay\n${_e}"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
-# git clone https://aur.archlinux.org/yay.git
-# cd yay
-# makepkg -si
+echo -e "\n${_y}Instalando google-chrome\n${_e}"
+yay -S google-chrome --noconfirm
+
+
+
+
 
